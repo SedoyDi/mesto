1d76ba6fe8c3fd4c9bd9a70d7f8bdfc9acd60eb4
Sedov Dmitry <SedoyDi@yandex.ru>
2022-02-27 11:47:58 +0300

    fit:addJs

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// работа popup
+const openButton = document.querySelector('.profile__edit');
+const closeButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+
+function popupOpen() {
+    popup.classList.add('popup_opened');
+};
+
+function popupClose() {
+    popup.classList.remove('popup_opened');
+}
+
+openButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+
+// работа form
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__form_nickname-input');
+let jobInput = document.querySelector('.popup__form_profession-input');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    let nickname = document.querySelector('.profile__nickname');
+    let profesion = document.querySelector('.profile__profession');
+    nickname.textContent = nameInput.value;
+    profesion.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
